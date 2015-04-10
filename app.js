893d4c90e2f79aedbb9a57c63b292b47f1b8d318
wesmangum <wesmangum24@gmail.com>
2015-04-09 23:35:42 -0500

    switch to handlebars

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 var express = require('express'),
 	app = express(),
 	path = require('path'),
+	exphbs = require('express-handlebars'),
 	initRoutes = require(__dirname + '/lib/init-routes.js')
 ;
 
 // views as directory for all template files
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 /**
  * Pipleline
